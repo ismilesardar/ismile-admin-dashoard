19bdf71a51998f602bbd53843a8af202c5b18799
Ismile Sardar <ismailsardar540@gmail.com>
2023-07-09 15:53:29 +0600

    index.jsx this file create to easy to import all page file

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,15 @@
+export { default as Register } from './Register';
+export { default as Login } from './Login';
+export { default as Ecommerce } from './Ecommerce';
+export { default as Orders } from './Orders';
+export { default as Customers } from './Customers';
+export { default as Product } from './Product';
+export { default as Area } from './Charts/Area';
+export { default as Bar } from './Charts/Bar';
+export { default as ColorMapping } from './Charts/ColorMapping';
+export { default as Financial } from './Charts/Financial';
+export { default as Line } from './Charts/Line';
+export { default as Pie } from './Charts/Pie';
+export { default as Pyramid } from './Charts/Pyramid';
+export { default as Stacked } from './Charts/Stacked';
+
